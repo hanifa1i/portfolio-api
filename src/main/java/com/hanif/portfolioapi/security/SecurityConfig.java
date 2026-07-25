8718961bdf1651e142b4dcd33ae038b166aecb4d
hanifali <ali.hanif@outlook.com>
2026-07-25 23:51:59 +0100

    granting access to api service in render environment

--- src/main/java/com/hanif/portfolioapi/security/SecurityConfig.java
+++ src/main/java/com/hanif/portfolioapi/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://portfolio-frontend-mu-self.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
